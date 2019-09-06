6c67b7c717321e7da3b3726c60125d152e5bb1f6
Mexaneg <54392167+Mexaneg@users.noreply.github.com>
2019-09-06 11:46:12 +0300

    try to used multithreads

--- com.queue/src/main/java/main.java
+++ com.queue/src/main/java/main.java
@@ -1,11 +1,26 @@
+import threads.*;
+
 public class main {
     static SomeThing mThing;
 
     public static void main(String[] args) {
-        mThing = new SomeThing();
+       /* mThing = new SomeThing();
         Thread myThread = new Thread(mThing);
         myThread.start();
 
         System.out.println("Главный поток завершен");
+                */
+        //one thread
+        //LiftOff launch = new LiftOff();
+        //launch.run();
+
+        //multithread
+        for (int i =0; i<5; i++){
+            Thread t = new Thread(new LiftOff());
+            t.start();
+        }
+
+        System.out.println("Waithing LiftOff");
+
     }
 }
